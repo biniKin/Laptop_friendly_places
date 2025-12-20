c05c944784f0c2664b3d42fca44ff90603b06a4e
biniKin <biniyamKinfe21@gmail.com>
2025-12-19 23:45:07 -0800

    finishing places page

---
+++ js/firebase/config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyBjxYXdJFVMvBZj3ssl7adBGd8C1QMGx1k",
+    authDomain: "task10auth.firebaseapp.com",
+    projectId: "task10auth",
+    storageBucket: "task10auth.firebasestorage.app",
+    messagingSenderId: "748554014277",
+    appId: "1:748554014277:web:7c02faf0a011dbdffd1d2c"
+};
--- js/places.js
+++ js/places.js
@@ -1,20 +1,66 @@
-
-
 import { auth, db } from "./firebase/init.js";
-import { getDocs, collection } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-firestore.js";
+import {
+  getDocs,
+  collection
+} from "https://www.gstatic.com/firebasejs/12.7.0/firebase-firestore.js";
 
-const container = document.querySelector(".places-container");
+/* -------------------- DOM -------------------- */
+
+const tabs = document.querySelectorAll(".tab");
+const loader = document.getElementById("loading");
+
+
+const containers = {
+  all: document.getElementById("all"),
+  new: document.getElementById("new"),
+  popular: document.getElementById("popular")
+};
+
+
+
+/* -------------------- STATE -------------------- */
+
+let allPlaces = [];
+
+
+function getFavorites() {
+  return JSON.parse(localStorage.getItem("favorites")) || [];
+}
+
+function saveFavorites(favorites) {
+  localStorage.setItem("favorites", JSON.stringify(favorites));
+}
+
+function isFav(id) {
+  return getFavorites().some(p => p.id === id);
+}
+
+function toggleFavorite(place) {
+  let favorites = getFavorites();
+
+  const index = favorites.findIndex(p => p.id === place.id);
+
+  if (index === -1) {
+    favorites.push(place); // add
+  } else {
+    favorites.splice(index, 1); // remove
+  }
+
+  saveFavorites(favorites);
+}
+
+
+/* -------------------- UI -------------------- */
 
 function createCard(place) {
   const card = document.createElement("div");
-
   card.className = "place-card";
 
   card.innerHTML = `
     <div class="place-image">
       <img src="${place.media.images[0]}" alt="${place.name}">
-      <button class="favorite-btn">
-        <i class="fa-regular fa-heart"></i>
+      <button class="favorite-btn" id="fav-btn">
+        <i class="${isFav(place.id) ?"fa-solid" : "fa-regular" } fa-heart"></i>
       </button>
     </div>
 
@@ -26,13 +72,13 @@ function createCard(place) {
       </div>
 
       <div class="place-features">
-        <span class="feature" id="wifi-feature" title="Wifi rating">
+        <span class="feature wifi-feature" title="Wifi rating">
           <i class="fa-solid fa-wifi"></i> ${place.rating.wifi}
         </span>
-        <span class="feature" id="power-feature" title="Power rating">
+        <span class="feature power-feature" title="Power rating">
           <i class="fa-solid fa-plug"></i> ${place.rating.power}
         </span>
-        <span class="feature" id="customer-feature" title="Customer service rating">
+        <span class="feature customer-feature" title="Customer service rating">
           <i class="fa-solid fa-face-smile"></i> ${place.rating.customer_service}
         </span>
       </div>
@@ -40,28 +86,71 @@ function createCard(place) {
       <p class="place-address">${place.description}</p>
     </div>
   `;
+  const favBtn = card.querySelector(".favorite-btn");
+  const icon = favBtn.querySelector("i");
+
+  
+favBtn.addEventListener("click", () => {
+  toggleFavorite(place);
+
+  icon.classList.toggle("fa-solid");
+  icon.classList.toggle("fa-regular");
+});
 
   return card;
 }
 
+/* -------------------- RENDER -------------------- */
+
+function renderPlaces(type) {
+  // clear all containers
+  Object.values(containers).forEach(c => (c.innerHTML = ""));
+
+  allPlaces.forEach(place => {
+    if (type === "all" || place.tag === type) {
+      containers[type].appendChild(createCard(place));
+    }
+  });
+}
+
+/* -------------------- TABS -------------------- */
+
+tabs.forEach(tab => {
+  tab.addEventListener("click", () => {
+    tabs.forEach(t => t.classList.remove("active"));
+    tab.classList.add("active");
+
+    const type = tab.dataset.target;
+    renderPlaces(type);
+  });
+});
+
+
+/* -------------------- FIREBASE -------------------- */
+
 async function fetchPlaces() {
   try {
+    loader.style.display = "flex";
     const placesCol = collection(db, "places");
     const snapshot = await getDocs(placesCol);
 
-    const places = snapshot.docs.map(doc => ({
+    allPlaces = snapshot.docs.map(doc => ({
       id: doc.id,
       ...doc.data()
     }));
 
-    places.forEach(place => {
-      const card = createCard(place);
-      container.appendChild(card); 
-    });
+    // initial render
+    loader.style.display = "none";
+    renderPlaces("all");
+
+    console.log(getFavorites());
 
   } catch (error) {
     console.error("Error fetching places:", error);
   }
 }
+
+
+
 
 fetchPlaces();
